20fb04619c86bd56e1f71ad418585b44716e3a1a
Michal Pietrzak <michalpietrzak@gmail.com>
2015-05-20 10:34:07 +0200

    dziala zmieniona funkcja dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,8 @@
-def dodawanie():
+def dodawanie(a,b):
     return a+b
 print('Podaj pierwsza liczbe:')
 a=int(input())
 print('Podaj druga liczbe:')
 b=int(input())
-dodawanie()
+wynik=dodawanie(a,b)
+print(wynik)
